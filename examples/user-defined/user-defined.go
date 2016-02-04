5cc692d392d6f3e90af5241e60e5507a9cafa287
Xing Xing <mikespook@gmail.com>
2016-02-04 16:53:52 +1300

    examples

--- examples/user-defined/user-defined.go
+++ examples/user-defined/user-defined.go
@@ -8,33 +8,67 @@ import (
 )
 
 type myRole struct {
-	gorbac.Role
+	*gorbac.StdRole
 	Label       string
 	Description string
 }
 
 func loadByName(name string) (label, description string) {
 	// loading data from storages or somewhere
-	return name, "This is the description for " + name
+	return name + " for testing", "This is the description for " + name
 }
 
-func newMyRole(rbac *gorbac.RBAC, name string) gorbac.Role {
+func newMyRole(name string) gorbac.Role {
 	// loading extra properties by `name`.
 	label, desc := loadByName(name)
 	role := &myRole{
-		Role:        gorbac.NewBaseRole(rbac, name),
 		Label:       label,
 		Description: desc,
 	}
+	role.StdRole = gorbac.NewStdRole(name).(*gorbac.StdRole)
 	return role
 }
 
 func main() {
-	rbac := gorbac.NewWithFactory(newMyRole)
-	rbac.Add("role-1", []string{"a", "b", "c"}, nil)
-	role := rbac.Get("role-1")
+	rbac := gorbac.New()
+	r1 := newMyRole("role-1")
+	r2 := newMyRole("role-2")
+	r3 := newMyRole("role-3")
+	r4 := newMyRole("role-4")
+	if err := rbac.Add(r1); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	if err := rbac.Add(r2); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	if err := rbac.Add(r3); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	if err := rbac.Add(r4); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	if err := rbac.SetParents("role-1", []string{"role-2", "role-3"}); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	if err := rbac.SetParent("role-3", "role-4"); err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+
+	role, parents, err := rbac.Get("role-1")
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
 	if myRole, ok := role.(*myRole); ok {
-		fmt.Printf("Name:\t%s\nLabel:\t%s\nDesc:\t%s\n",
-			myRole.Name(), myRole.Label, myRole.Description)
+		fmt.Printf("Name:\t%s\nLabel:\t%s\nDesc:\t%s\nParents:\t%s\n",
+			myRole.Id(), myRole.Label, myRole.Description, parents)
 	}
 }
